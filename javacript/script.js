0ed8b05838059dc679ad8967f8c5a33ece9efc2d
Arthur <ArthurHallack2@gmail.com>
2023-08-20 23:48:47 -0300

    TodoList

--- javacript/script.js
+++ javacript/script.js
@@ -7,23 +7,37 @@ const EditInput = window.document.querySelector("#Edit-input");
 const cancelEditBTN = window.document.querySelector("#cancelEditBTN");
 
 //funções//
-const savetd = (Text) => {
+const saveTodo = (text) => {
+    const todo = window.document.createElement ("div")
+    todo.classList.add("todo")
 
-    const td = document.createElement ("div")
-    td.classList.add("td")
-    const tdTitle = document.createElement("h3")
-    tdTitle.innerText = Text
+    const todoTitle = document.createElement ("h3")
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
 
+    const doneBtn = window.document.createElement ("button")
+    doneBtn.classList.add("finish-td")
+    doneBtn.innerHTML ='<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+    
+    const EditBtn = window.document.createElement ("button")
+    EditBtn.classList.add("edit-td")
+    EditBtn.innerHTML ='<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(EditBtn)
+
+    const DeleteBtn = window.document.createElement ("button")
+    DeleteBtn.classList.add("remove-td")
+    DeleteBtn.innerHTML ='<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(DeleteBtn)
+
+    TodoList.appendChild(todo)
 }
-
-}
-
 
 //eventos//
 tdForm.addEventListener("submit", (e) => {
     e.preventDefault()
     const inputvalue = tdInput.value
     if (inputvalue) {
-        savetd(inputvalue)
+        saveTodo(inputvalue)
     }
 });
